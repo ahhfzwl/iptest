f1ac384cbd709261c8280528b5104833246984f0
ahhfzwl <ahhfzwl@qq.com>
2023-12-15 00:13:50 +0800

    Update iptest.sh

--- iptest.sh
+++ iptest.sh
@@ -5,8 +5,6 @@ TLS=$1
 if [[ -z "$TLS" ]]; then
   TLS=true
 else
-  TLS=FALSE
+  TLS=false
 fi
-echo "$TLS"
-
 ./iptest -file=ip.txt -speedtest=4 -tls=$TLS -url=speed.sock.cf/__down?bytes=500000000

01862c9d69eb6678f6a8898c60f04661b77ad7bd
ahhfzwl <ahhfzwl@qq.com>
2023-12-15 00:12:41 +0800

    Update iptest.sh

--- iptest.sh
+++ iptest.sh
@@ -9,4 +9,4 @@ else
 fi
 echo "$TLS"
 
-/ahzwl/iptest/iptest-linux-amd64 -file=/ahzwl/iptest/ip.txt -speedtest=4 -tls=$TLS -url=speed.sock.cf/__down?bytes=500000000
+./iptest -file=ip.txt -speedtest=4 -tls=$TLS -url=speed.sock.cf/__down?bytes=500000000
